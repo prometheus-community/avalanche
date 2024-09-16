228f147f655b1238da1b597fb1e27b5fddcfcd59
Vanshika <102902652+Vanshikav123@users.noreply.github.com>
2024-09-16 13:30:59 +0530

    Out-of-order Flag : Added option for sending out of order samples (#68)
    * changed implementation of shuffleTimeStamps
    
    
    * resolving conflicts and improve test
    
    
    ---------

--- cmd/avalanche.go
+++ cmd/avalanche.go
@@ -56,6 +56,7 @@ var (
 	remoteTenant         = kingpin.Flag("remote-tenant", "Tenant ID to include in remote_write send").Default("0").String()
 	tlsClientInsecure    = kingpin.Flag("tls-client-insecure", "Skip certificate check on tls connection").Default("false").Bool()
 	remoteTenantHeader   = kingpin.Flag("remote-tenant-header", "Tenant ID to include in remote_write send. The default, is the default tenant header expected by Cortex.").Default("X-Scope-OrgID").String()
+	outOfOrder           = kingpin.Flag("out-of-order", "Enable out-of-order timestamps in remote write requests").Default("true").Bool()
 )
 
 func main() {
@@ -123,6 +124,7 @@ func main() {
 				InsecureSkipVerify: *tlsClientInsecure,
 			},
 			TenantHeader: *remoteTenantHeader,
+			OutOfOrder:   *outOfOrder,
 		}
 
 		// Collect Pprof during the write only if not collecting within a regular interval.
@@ -136,7 +138,7 @@ func main() {
 		)
 		if *remotePprofInterval > 0 {
 			if len(*remotePprofURLs) == 0 {
-				log.Fatal("remote profiling interval specified wihout any remote pprof urls")
+				log.Fatal("remote profiling interval specified without any remote pprof urls")
 			}
 			suffix := rand.Intn(1000)
 			go func() {
@@ -170,7 +172,7 @@ func main() {
 		return
 	}
 
-	fmt.Printf("Serving ur metrics at localhost:%v/metrics\n", *port)
+	fmt.Printf("Serving your metrics at localhost:%v/metrics\n", *port)
 	err = metrics.ServeMetrics(*port)
 	if err != nil {
 		log.Fatal(err)
