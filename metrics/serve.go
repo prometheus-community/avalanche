7e2ed921a31193e74993dd7cf2430b581e633573
Prateek <prateek@bluekanvas.com>
2025-01-15 18:34:42 +1100

    Remove the accidental merge conflict
    Signed-off-by: Prateek <prateek@bluekanvas.com>

--- metrics/serve.go
+++ metrics/serve.go
@@ -505,7 +505,6 @@ func (c *Collector) Run() error {
 		labelValues = append(labelValues, split[1])
 	}
 
-	c.labelKeys = labelKeys
 	mutableState := &metricState{seriesCount: c.cfg.SeriesCount, labelValues: labelValues}
 	// unsafe means you need to lock c.mu to use it.
 	unsafeReadOnlyGetState := func() metricState { return *mutableState }
